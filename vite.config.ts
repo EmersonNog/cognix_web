9d90648213e9f82375caed845b80ac54611ef394
Emerson Nogueira <catce.nogueira@gmail.com>
2026-04-07 22:31:05 -0300

    fix: add base em vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/cognix_web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
